e539a548b3db404cba1ad90416b410aa4cd4f242
Sơn Hellu <sonhellu@MacBook-Pro-cua-Son.local>
2025-10-17 13:34:22 +0900

    update folder

---
+++ backend/README.md
@@ -0,0 +1,4 @@
+# hello_campus
+
+Backend-Project
+
---
+++ frontend/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods-Runner-dummy.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/sonhellu/Downloads/Capstone/Test2/hello_campus/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
+  /Users/sonhellu/Downloads/Capstone/Test2/hello_campus/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks/Foundation.framework/Modules/module.modulemap
